f508a7bda78ac20b0b30b06f1fcc8e6019935f3f
Dimasgantengamat <dimaswirapratama707@gmail.com>
2024-12-03 14:58:18 +0700

    init

---
+++ Latihan Java/kano.java
@@ -0,0 +1,15 @@
+public class kano {
+    public static void main(String[] args) {
+    
+        String Nama = ("Kano Marzuq Permata");
+        int Tinggi = (150);
+        double berat = (50.5);
+        int IQ = (76);
+
+        System.out.println("Nama = " + Nama);
+        System.out.println("Tinggi = " + Tinggi + " cm");
+        System.out.println("berat = " + berat + " kg");
+        System.out.println("Kepintaran IQ Kano = " + IQ);
+        System.out.println("Kano anak Bigal buyan, belum berak, belum ngising, belum sholat, belum makan");
+    }
+}
